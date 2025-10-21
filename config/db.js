b35cd71816f7f4af67d267a82a7b8bc359f97e0a
sprocketcipher <mandy.siragusa@gmail.com>
2025-10-21 10:55:12 -0500

    Database updates (Supabase and Prisma)

--- config/db.js
+++ config/db.js
@@ -3,3 +3,13 @@
  * DB connection (e.g., MongoDB, PostgreSQL)
  *
  *  */
+
+//export generatorclient = {
+//  provider = "prisma-client-js"
+//}
+//
+//datasource db {
+//  provider  = "postgresql"
+//  url       = env("DATABASE_URL")
+//  directUrl = env("DIRECT_URL")
+//}
