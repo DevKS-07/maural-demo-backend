3cf7b4b42c5cd9b69a559de1b8f19109447dec30
s-tus <saintus-s@webmail.uwinnipeg.ca>
2025-10-21 16:00:05 +0000

    prisma connection and schemas

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const routes = require("./routes");
 
+
 const app = express();
 
 // Middlewares
@@ -15,6 +16,7 @@ app.use(morgan("combined")); // for logging HTTP requests
 
 // Routes
 app.use("/", routes); // Use the routes defined in routes.js
+
 
 // #######################################################
 // ############# 404 & Error Handling Routes #############
